b2e9dee675eef9e7ebdeabaa65ee953d81894b1b
jiwei <jiwei@DESKTOP-5M51MMH.lan>
2017-09-27 22:12:43 +0800

    IU-2017.2.4 <jiwei@DESKTOP-5M51MMH Create project.default.xml, Enum.java, laf.xml, Interface.java, other.xml, Class.java, jdk.table.xml, Default.xml, path.macros.xml, ui.lnf.xml, colors.scheme.xml, databaseSettings.xml, notifications.xml, editor.xml, mavenVersion.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 
+ * User: ${USER}
+ * Date: ${YEAR}-${MONTH}-${DAY}
+ * Time: ${TIME}
+ */
+public class ${NAME} {
+}
